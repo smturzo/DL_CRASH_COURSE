68966dca163c041d57f150006a12f5fa9f841e4e
Bargeen Turzo <osu10269@pitzer-rw02.ten.osc.edu>
2022-09-18 23:12:21 -0400

    I think this branch is deleted

--- Bargeen/Week1/tutorial.py
+++ Bargeen/Week1/tutorial.py
@@ -18,6 +18,10 @@ img_tensor, label = dataset[0]
 print(img_tensor.shape, label)
 
 from torch.utils.data import random_split
-train_ds, val_ds = random_split(dataset, [50000, 10000])
+train_ds, val_ds = random_split(dataset, [50000, 10000]) #Hyper parameters
 print("Size of training set: ", len(train_ds), "Size of validation set: ", len(val_ds))
 
+from torch.utils.data import DataLoader
+batch_size = 128 #Hyper parameter
+train_loader = DataLoader(train_ds, batch_size, shuffle=True)
+val_loader = DataLoader(val_ds, batch_size) # On the other hand, since the validation data loader is used only for evaluating the model, there is no need to shuffle the images. (Not sure I understand this explanation)
